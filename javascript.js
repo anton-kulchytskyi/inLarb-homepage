15e64df2261bce2b366f2993a5631b7164a562a2
anton-kulchytskyi <anton@oykos-hub.com>
2020-07-23 15:55:21 +0200

    market

--- javascript.js
+++ javascript.js
@@ -31,21 +31,9 @@ const slider = document.getElementById("slider"),
 
 next.addEventListener("click", e => {
   slider.scrollBy(width + gap, 0);
-  // if (carousel.scrollWidth !== 0) {
-  //   prev.style.display = "flex";
-  // }
-  // if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-  //   next.style.display = "none";
-  // }
 });
 prev.addEventListener("click", e => {
   slider.scrollBy(-(width + gap), 0);
-  // if (carousel.scrollLeft - width - gap <= 0) {
-  //   prev.style.display = "none";
-  // }
-  // if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-  //   next.style.display = "flex";
-  // }
 });
 
 let width = slider.offsetWidth;
